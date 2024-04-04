5b1892cf50539f4101271194ee2b77afc09d05de
Maria Knorps <maria.knorps@tweag.io>
2024-04-04 09:11:26 +0200

    Add outline of extract and prepare modules

--- rag_experiment/extract.py
+++ rag_experiment/extract.py
@@ -7,6 +7,10 @@ import tarfile
 # Construct the default API client.
 client = arxiv.Client()
 
+# TODO Function that takes query as an argument for arxiv search and returns the results
+def search_arxiv(query: str) -> arxiv.Search:
+    pass
+
 # Search for the 10 most recent articles matching the keyword "quantum."
 search = arxiv.Search(
   query = "safety in AI",
@@ -15,3 +19,15 @@ search = arxiv.Search(
 )
 
 results = client.results(search)
+
+# TODO Download results to a separate folder. Create function that takes results and name of the folder as an input.
+# This should be a temporary folder. 
+def download_results(results: arxiv.Results, folder: str):
+    pass
+
+
+# TODO Unpack dowloaded archives, but only .tex files and store them in a separate folder.
+def unpack_archives(folder: str):
+    pass
+
+
--- rag_experiment/prepare.py
+++ rag_experiment/prepare.py
@@ -2,10 +2,14 @@
 import pydetex.pipelines as pp
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-# test of pydetex library
+# TODO Create a funtion that takes a LaTeX file name and tranforms it into plain text
 with open("./papers/test1/Sections/Experiments.tex") as f:
      latex = f.read()
 
 text = "This is a \\textbf{LaTex} code..."
 out = pp.strict(latex)
 print(out)
+
+# TODO Cretae a function that traverses all files in a given directory and runs transformation to plain text function
+
+# TODO Create a function that takes a plain text and splits it into sentences
