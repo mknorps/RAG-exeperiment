84994c162aed4c56436d449d6f74daf2dfb81670
marcin-zadrozny <mzadrozn37y@gmail.com>
2024-04-29 10:08:36 +0200

    added metadata extraction to extract.py

--- rag_experiment/extract.py
+++ rag_experiment/extract.py
@@ -5,6 +5,7 @@ import tempfile
 import gzip
 import os
 import shutil
+import json
 
 # Construct the default API client.
 client = arxiv.Client()
@@ -26,12 +27,26 @@ def download_results(results: arxiv.Result, temp_dir: str):
         try:
             paper.download_source(temp_dir)
         except Exception as e:
-            print(f"Failed to download {paper.entry_id} due to {e}")        
+            print(f"Failed to download {paper.entry_id} due to {e}")  
+
+# Make a list or dictionaries containing metadata from papers
+def extract_metadata(results: arxiv.Result) -> list:
+    metadata_list = []
+    for paper in results:
+        metadata = {
+            'authors': [author.name for author in paper.authors],
+            'title': paper.title,
+            'published': paper.published.strftime('%Y-%m-%d'),
+            'domain': paper.primary_category,
+            'tags': paper.categories
+            }
+        metadata_list.append(metadata)
+    return metadata_list
         
         
 # Unpack dowloaded archives, but only .tex files and store them in a separate folder.
-def unpack_archives(source_dir: str, target_dir: str):
-    for file_name in os.listdir(source_dir):
+def unpack_archives(source_dir: str, target_dir: str, metadata_list: list):
+    for i, file_name in enumerate(os.listdir(source_dir)):
         if file_name.endswith('.tar.gz'):
             # Create a unique subdirectory for each archive
             subdirectory_path = os.path.join(target_dir, os.path.splitext(file_name)[0])
@@ -44,7 +59,11 @@ def unpack_archives(source_dir: str, target_dir: str):
                     # Extract tex files
                     for member in tex_files:
                         member.name = os.path.basename(member.name)
-                        tar.extract(member, path=subdirectory_path) 
+                        tar.extract(member, path=subdirectory_path)
+                        
+                with open(os.path.join(subdirectory_path, 'metadata.json'), 'w') as f:
+                    json.dump(metadata_list[i], f, indent=4)
+
             except (tarfile.ReadError, gzip.BadGzipFile) as e:
                 print(f"Skipping file {file_name} due to error: {e}")
 
@@ -58,16 +77,19 @@ def extract_tex_files(query: str, target_dir: str, max_results: int = 10):
 
     try:
         # Search arXiv for the query
-        results = search_arxiv(query, max_results)
+        results = list(search_arxiv(query, max_results))
         
         # Download the papers' source files to the temporary directory
         download_results(results, temp_dir)
+        
+        # Create metadata
+        metadata_list = extract_metadata(results)
 
         # Ensure the target directory exists
         os.makedirs(target_dir, exist_ok=True)
 
-        # Unpack .tex files from the downloaded tar.gz files to the target directory
-        unpack_archives(temp_dir, target_dir)
+        # Unpack .tex files from the downloaded tar.gz files to the target directory and add metadata.json for each paper
+        unpack_archives(temp_dir, target_dir, metadata_list)
 
     finally:
         # Cleanup: Remove the temporary directory and all its contents
@@ -78,4 +100,4 @@ def extract_tex_files(query: str, target_dir: str, max_results: int = 10):
 
 # Example usage:
 if __name__ == "__main__":
-    extract_tex_files(query="safety in AI", max_results=5, target_dir="papers_example")
+    extract_tex_files(query="safety in AI", target_dir="papers_example", max_results=10)
