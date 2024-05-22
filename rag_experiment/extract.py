e88a148d3ae1229ef2feb6487e3110065fbf9e59
marcin-zadrozny <mzadrozn37y@gmail.com>
2024-05-22 15:32:49 +0200

    improving metadata extraction

--- rag_experiment/extract.py
+++ rag_experiment/extract.py
@@ -29,40 +29,53 @@ def download_results(results: arxiv.Result, temp_dir: str):
         except Exception as e:
             print(f"Failed to download {paper.entry_id} due to {e}")  
 
-# Make a list or dictionaries containing metadata from papers
-def extract_metadata(results: arxiv.Result) -> list:
-    metadata_list = []
+# Make a dict containing metadata from papers
+def extract_metadata(results: arxiv.Result) -> dict:
+    metadata = dict()
+    
     for paper in results:
-        metadata = {
+    
+        metadata[paper.entry_id.split('/')[-1]] = {
             'authors': [author.name for author in paper.authors],
             'title': paper.title,
             'published': paper.published.strftime('%Y-%m-%d'),
             'domain': paper.primary_category,
-            'tags': paper.categories
-            }
-        metadata_list.append(metadata)
-    return metadata_list
-        
+            'tags': paper.categories,
+            'entry_id': paper.entry_id,
+            'update_date': paper.updated.strftime('%Y-%m-%d'),
+            'journal_ref': paper.journal_ref,
+            'doi': paper.doi,
+            'links': [{"url": link.href, "title": link.title, "rel": link.rel,
+                    "content_type": link.content_type} for link in paper.links]
+        }
+    return metadata
+
         
 # Unpack dowloaded archives, but only .tex files and store them in a separate folder.
-def unpack_archives(source_dir: str, target_dir: str, metadata_list: list):
-    for i, file_name in enumerate(os.listdir(source_dir)):
+# Plus add metadata.json for each paper
+def unpack_archives(source_dir: str, target_dir: str, metadata_dict: dict):
+    for file_name in os.listdir(source_dir):
         if file_name.endswith('.tar.gz'):
             # Create a unique subdirectory for each archive
             subdirectory_path = os.path.join(target_dir, os.path.splitext(file_name)[0])
             os.makedirs(subdirectory_path, exist_ok=True)  # Ensure the subdirectory exists
+            
+            # Extract entry_id from the file name (the format is entry_id.sanitized _title.tar.gz)
+            entry_id = '.'.join(file_name.split('.')[0:2])
+            
             try:
+                file_path = os.path.join(source_dir, file_name)
                 # Open the tar.gz file
-                with tarfile.open(os.path.join(source_dir, file_name), 'r:gz') as tar:
+                with tarfile.open(file_path, 'r:gz') as tar:
                     # Filter for .tex files and extract them
                     tex_files = [member for member in tar.getmembers() if member.name.endswith('.tex')]
                     # Extract tex files
                     for member in tex_files:
                         member.name = os.path.basename(member.name)
                         tar.extract(member, path=subdirectory_path)
-                        
+                
                 with open(os.path.join(subdirectory_path, 'metadata.json'), 'w') as f:
-                    json.dump(metadata_list[i], f, indent=4)
+                    json.dump(metadata_dict[entry_id], f, indent=4)
 
             except (tarfile.ReadError, gzip.BadGzipFile) as e:
                 print(f"Skipping file {file_name} due to error: {e}")
@@ -83,13 +96,13 @@ def extract_tex_files(query: str, target_dir: str, max_results: int = 10):
         download_results(results, temp_dir)
         
         # Create metadata
-        metadata_list = extract_metadata(results)
+        metadata_dict = extract_metadata(results)
 
         # Ensure the target directory exists
         os.makedirs(target_dir, exist_ok=True)
 
         # Unpack .tex files from the downloaded tar.gz files to the target directory and add metadata.json for each paper
-        unpack_archives(temp_dir, target_dir, metadata_list)
+        unpack_archives(temp_dir, target_dir, metadata_dict)
 
     finally:
         # Cleanup: Remove the temporary directory and all its contents
